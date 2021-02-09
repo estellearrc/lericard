correct heading with magic formula

src/Boat.py
@@ -8,7 +8,7 @@ import time
 
 
 def sawtooth(x):
-    return (x+2*np.pi) % (2*np.pi)-np.pi
+    return (x+np.pi) % (2*np.pi)-np.pi
 
 
 class Boat:
@@ -20,11 +20,16 @@ class Boat:
         # Compass calibration
         # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
         self.bus = smbus.SMBus(1)
-        # Calibrage de la boussole
-        x1 = np.array([[-2388], [-1892], [1668]])
-        x_1 = np.array([[2870], [-2650], [1404]])
-        x2 = np.array([[-50], [-5430], [3447]])
-        x3 = np.array([[300], [-2020], [6780]])
+        # # Calibrage de la boussole boat 14
+        # x1 = np.array([[-2388], [-1892], [1668]])
+        # x_1 = np.array([[2870], [-2650], [1404]])
+        # x2 = np.array([[-50], [-5430], [3447]])
+        # x3 = np.array([[300], [-2020], [6780]])
+        # Calibrage de la boussole boat 15
+        x1 = np.array([[-5494], [1018], [-215]])
+        x_1 = np.array([[499], [268], [-2538]])
+        x2 = np.array([[-1518], [-2066], [-2431]])
+        x3 = np.array([[-1692], [626], [2735]])
         self.compass = Compass(self.bus, x1, x_1, x2, x3)
         self.motors = Motors()
         self.gps = GPS()

src/Compass.py
@@ -5,6 +5,10 @@ import smbus
 import time
 
 
+def sawtooth(x):
+    return (x+np.pi) % (2*np.pi)-np.pi
+
+
 def merge(lower_byte, upper_byte):
     """merge 2 bytes (2*8 bits) to form a 16-bit long binary integer"""
     res = lower_byte + upper_byte*256
@@ -69,6 +73,10 @@ class Compass:
         B = np.linalg.inv(self.A)@(X + self.b)
         return B
 
+    def compute_heading(self, Bx, By):
+        """Magic formula"""
+        return sawtooth(np.arctan2(Bx, By) + np.pi/2)
+
 
 def test():
     """ Retrieve compass measures x1, x_1, x2, x3 for earth magnetic field"""
@@ -80,7 +88,8 @@ def test():
         bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG3, 0b00000000)
         six_values = bus.read_i2c_block_data(DEVICE_ADDRESS, OUT_X_L, 6)
         x, y, z = convert(six_values)
-        print("[Bx, By, Bz] = [%f,%f,%f] = ", x, y, z)
+        print("[Bx, By, Bz] = [{}, {}, {}] = ".format(x, y, z))
+        print("cap = ", sawtooth(np.arctan2(Bx, By) + np.pi/2))
         time.sleep(1)
 
 