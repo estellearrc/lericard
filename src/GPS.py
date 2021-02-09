20f9e368af25749af9f794b2d08844e0391300d2
MaxLgy <maxime.legeay@ensta-bretagne.org>
2021-02-09 15:21:38 +0100

    writing traceback with GPS

--- src/GPS.py
+++ src/GPS.py
@@ -9,16 +9,20 @@ class GPS:
     # precision de 1.9m au bout du ponton
     lx0 = 48.19906500
     ly0 = -3.01473333
+    file_name = "GPS_traceback.txt"
 
     def __init__(self):
         self.range = 5
         self.gps_com = gpsdrv.init_line()
+        f = open(file_name, "w")
+        f.close()
 
     def read_sensor_values(self):
         data = gpsdrv.read_gll(self.gps_com)
         # divide by 100 to be in degrees
         data[0] = data[0]/100  # North
-        data[2] = -data[2]/100  # West = -Est$
+        data[2] = -data[2]/100  # West = -Est
+        self.write_coordinates(data[0], data[2])
         return data
 
     def destroy(self):
@@ -34,7 +38,11 @@ class GPS:
         lx = data_array[0]  # North / longitude
         ly = data_array[2]  # West / latitude
         return self.convert_to_cart_coord(lx, ly)
-
+        
+    def write_coordinates(self, lon, lat):
+        with open(filename, "a") as f:
+            f.write(str(lon) + " " + str(lat))
+            
 
 def test():
     """ Try GPS """
