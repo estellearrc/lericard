32045084cf7e07b0d1475b8d00d4d3385327d76f
estellearrc <estelle.arricau@gmail.com>
2021-02-10 15:33:38 +0100

    GPS retroactive change

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import fsm
 import numpy as np
 import sys
 import Boat
+from GPS import convert_DDmm_to_rad
 
 
 remaining_points = []
@@ -46,14 +47,13 @@ def doTriangle():
     y_2 = -3.016188
     x_3 = 48.200187
     y_3 = -3.015764
-    X1 = boat.gps.convert_to_cart_coord(x_1, y_1)
-    X2 = boat.gps.convert_to_cart_coord(x_2, y_2)
-    X3 = boat.gps.convert_to_cart_coord(x_3, y_3)
+    x_1, y_1 = convert_DDmm_to_rad(x_1, y_1)
+    x_2, y_2 = convert_DDmm_to_rad(x_2, y_2)
+    x_3, y_3 = convert_DDmm_to_rad(x_3, y_3)
     print("A : x=", x_1, " y=", y_1)
     print("B : x=", x_2, " y=", y_2)
     print("C : x=", x_3, " y=", y_3)
-    remaining_points = [(X1[0, 0], X1[1, 0]), (X2[0, 0], X2[1, 0]),
-                        (X3[0, 0], X3[1, 0]), (X1[0, 0], X1[1, 0])]
+    remaining_points = [(x_1, y_1), (x_2, y_2), (x_3, y_3), (x_1, y_1)]
     x_target, y_target = remaining_points.pop(0)
     target_point = np.array([[x_target], [y_target]])
     print("Going to point x=", x_target, " y=", y_target)
@@ -66,21 +66,21 @@ def doTriangle():
             x_target, y_target = remaining_points.pop(0)
             target_point = np.array([[x_target], [y_target]])
             print("Going to point x=", x_target, " y=", y_target)
-        
+
         # Guide block
         heading_obj = boat.compute_heading(target_point)
         print("obj : ", heading_obj)
         print("heading : ", heading)
-        
+
         v_obj = 120
-        
+
         # Control block
-        u_L, u_R = boat.follow_heading(heading, heading_obj, v_obj)      
-        
+        u_L, u_R = boat.follow_heading(heading, heading_obj, v_obj)
+
         # DDBoat command
         boat.motors.command(u_L, u_R)
         X = boat.compass.read_sensor_values().flatten().reshape((3, 1))
-    
+
     print("End of the triangle ...")
     return "stop"
 
@@ -92,23 +92,24 @@ def doGoNorth():
     while t - t0 < 1000:
         # Nav block
         heading = boat.compass.compute_heading(X[0, 0], X[1, 0])
-        
+
         # Guide block
         heading_obj = np.pi/2
         v_obj = 160
         t = time.time()
-        
-        #Control block
+
+        # Control block
         u_L, u_R = boat.follow_heading(heading, heading_obj, v_obj)
-        
+
         # DDBoat command
         boat.motors.command(u_L, u_R)
         # boat.motors.command(40, 40)
         X = boat.compass.read_sensor_values().flatten().reshape((3, 1))
-        
+
         with open("test_heading_following.csv", "a") as f:
-            f.write(str(t) + "," + str(heading) + "," + str(u_L) + "," + str(u_R) + "\n")
-    
+            f.write(str(t) + "," + str(heading) + "," +
+                    str(u_L) + "," + str(u_R) + "\n")
+
     event = "stop"
     print("Stop following North")
     return event
