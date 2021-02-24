a46abcc1fb6065bbf601599ea859e9b410b79527
MaxLgy <maxime.legeay@ensta-bretagne.org>
2021-02-24 17:51:26 +0100

    Data analysis update with minor bug resolution

--- src/logs/data_analysis.py
+++ src/logs/data_analysis.py
@@ -7,19 +7,21 @@ if __name__ == "__main__":
     with open(file_name, "r") as f:
         mission_name = str(f.readline().split("\n")[0])
         variables = f.readline().split(",")
+        variables.pop()
         values = {}
         for key in variables:
             values[key] = []
         
         for line in f.readlines():
             for i in range(len(variables)):
-                values[variables[i]].append(line.split(",")[i])
+                values[variables[i]].append(float(line.split(",")[i]))
 
     if mission_name == "triangle":
         fig, (ax1, ax2) = plt.subplots(2)
         fig.suptitle('Heading and motor commands : mission ' + mission_name)
-        ax1.plot(values['t'], values['heading_gps'], 'b-', label = 'heading route')
+        ax1.plot(values['t'], values['heading'], 'b-', label = 'heading route')
         ax1.plot(values['t'], values['heading_obj'], 'k--', label = 'heading objective')
+        
         
         ax2.plot(values['t'], values['u_L'], 'r-', label = 'left command')
         ax2.plot(values['t'], values['u_R'], 'g-', label = 'right command')
@@ -37,12 +39,47 @@ if __name__ == "__main__":
     elif mission_name == 'goNorth':
         fig, (ax1, ax2) = plt.subplots(2)
         fig.suptitle('Heading and motor commands : mission ' + mission_name)
-        ax1.plot(values['t'], values['heading_gps'], 'b-', label = 'heading route')
-        ax2.plot(values['t'], values['u_L'], 'r-', label = 'left command')
-        ax2.plot(values['t'], values['u_R'], 'g-', label = 'right command')
-        fig.xlabel('Time (s)')
-        ax2.ylabel('Motors commands (0-255)')
-        ax1.ylabel('Heading objective (rad)')
+        ax1.plot(values['heading'], 'b-', label = 'heading route')
+        ax2.plot(values['u_L'], 'r-', label = 'left command')
+        ax2.plot(values['u_R'], 'g-', label = 'right command')
+        ax2.set(ylabel = 'Motors commands (0-255)')
+        ax1.set(ylabel = 'Heading objective (rad)')
         fig.legend()
+    
+    elif mission_name == "test_GPS":
+        fig, (ax1, ax2) = plt.subplots(2)
+        fig.suptitle('Heading and speed : mission ' + mission_name)
+        ax1.plot(values['t'], values['speed'], 'b-', label = 'heading route')
+        
+        
+        ax2.plot(values['t'], values['heading'], 'r-', label = 'left command')
+        ax1.set(xlabel = 'Time (s)', ylabel = 'Speed (m/s)')
+        ax2.set(xlabel = 'Time (s)', ylabel = 'Heading (rad)')
+        fig.legend()
+        
+        plt.figure(2)
+        plt.plot(values['lat'], values['long'], 'b.')
+        plt.title("GPS route : mission " + mission_name)
+        plt.xlabel('Position x (m)')
+        plt.ylabel('Position y (m)')
+        plt.legend()    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
         
     plt.show()
